1fa5a31e011f6597034b031c629674813024d6db
Denise Pendleton <dpendl1@hotmail.com>
2018-04-20 00:51:57 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,12 +8,13 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+      binding.pry
     erb :index
   end
 
   post '/login' do
     @user = User.find_by(username: params["username"], password: params["password"])
-    binding.pry
+  
     if @user && @user.password == params[:password]
     session[:user_id] = @user.id
 
